7716e243a6a03eba104b6fa15b184e7f6de21d8e
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-10-20 19:03:04 -0400

    add simple starwars functionality

--- README.md
+++ README.md
@@ -1,22 +1,15 @@
-# 📊 Project: Simple API 1
+# 👽 StarWars API
+##  an app that displays all starwars characters
 
-### Goal: Display data returned from an api
+### Technologies used: 
+- HTML
+- CSS
+- JavaScript
+- StarWars API
 
-### How to submit your code for review:
+### How to use:
+- Click on the button
+- A random starwars character will be displayed
 
-- Fork and clone this repo
-- Create a new branch called answer
-- Checkout answer branch
-- Push to your fork
-- Issue a pull request
-- Your pull request description should contain the following:
-  - (1 to 5 no 3) I completed the challenge
-  - (1 to 5 no 3) I feel good about my code
-  - Anything specific on which you want feedback!
 
-Example:
-```
-I completed the challenge: 5
-I feel good about my code: 4
-I'm not sure if my constructors are setup cleanly...
-```
+
--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-const elt = document.getElementById('calculator');
-const calculator = Desmos.GraphingCalculator(elt);
-document.querySelector('#btn').addEventListener('click', showEquation); 
+document.querySelector('#btn').addEventListener('click', getRandomCharacter); 
 
-function showEquation() {
-  const equation = document.querySelector('#equation').value;
-  calculator.setExpression({ id: 'graph1', latex: equation });
-  console.log(calculator);
-  calculator.getState(); 
+async function getRandomCharacter() {
+
+  //fetch all the characters from the people endpoint
+  // const allChars = [];
+  // for (let i = 1; i <= 9; i++){
+  //   const url = `https://swapi.dev/api/people/?page=${i}`;
+  //   const currPageChars = await fetchChar(url);
+  //   allChars.push(...currPageChars.results);
+  // }
+  
+  // console.log(allChars); 
+  const randomNum = Math.floor(Math.random() * 82)
+  console.log(randomNum); 
+  const url = `https://swapi.dev/api/people/${randomNum}`;
+  const char = await fetchChar(url);
+  console.log(char);
+
+  const article = `<article class="card">
+  <div class="container">
+    <h4>${char.name}</h4> 
+    <p>Height: ${char.height}</p> 
+     <p>Mass: ${char.mass}</p> 
+     <p>Eye Color: ${char.eye_color}</p> 
+      <p>Hair Color: ${char.hair_color}</p> 
+       <p>Birth Year: ${char.birth_year}</p> 
+        <p>Gender: ${char.gender} </p> 
+
+  </div>
+</article>`;
+  
+  document.querySelector('.character').innerHTML = article;
+  
+
+  
 }
 
 
+async function fetchChar(url) {
+  try {
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.error(`Error fetching character status:${response.status}, ${response.statusText}`); 
+    }
+    const chars = await response.json(); 
+    return chars; 
+
+  } catch (error) {
+    console.error(error)
+  }
+}
 
